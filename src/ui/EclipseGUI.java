52e774019577029c4c07b164cc375614d2a2b689
SeanQO <seanquintero3@gmail.com>
2020-11-25 09:36:59 -0500

    Added GUI methods and atributes, and buttons in the fxml

--- src/ui/EclipseGUI.java
+++ src/ui/EclipseGUI.java
@@ -1,5 +1,29 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+
 public class EclipseGUI {
+
+    @FXML
+    private AnchorPane background;
+
+    @FXML
+    private Circle sun;
+
+    @FXML
+    private Circle moon;
+
+    @FXML
+    void startButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stopButton(ActionEvent event) {
+
+    }
 
 }

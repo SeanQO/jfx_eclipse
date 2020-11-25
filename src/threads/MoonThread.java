70c6e08decd0ab6b3d9c3f0473a31c70732c91a8
ander2812 <ander2812@gmail.com>
2020-11-25 10:02:27 -0500

    se modifico la clase MoonThread

--- src/threads/MoonThread.java
+++ src/threads/MoonThread.java
@@ -1,5 +1,16 @@
 package threads;
 
-public class MoonThread {
+import ui.EclipseGUI;
 
+public class MoonThread extends Thread {
+    private EclipseGUI eGUI;
+
+    public MoonThread(EclipseGUI eGUI) {
+        this.eGUI = eGUI;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
 }
